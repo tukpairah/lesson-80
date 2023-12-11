6ca02d01985094e56bcd07f34cd152deed7fc11b
tukpairah <136407516+tukpairah@users.noreply.github.com>
2023-12-11 21:51:47 +0500

    Add files via upload

---
+++ lesson 80/main.js
@@ -0,0 +1,27 @@
+for (let i = 2; i <= 12; i = i + 2) {
+console.log(`${i}`)
+}
+
+
+for (let i = 10; i >= 1; i--) {
+console.log(i)
+}
+
+
+let i = 2
+
+while (i < 12) {
+  i = i + 2
+  console.log(i)
+}
+
+
+let a = 11
+
+while (a >= 1) {
+  a--
+  console.log(a)
+}
+
+
+
